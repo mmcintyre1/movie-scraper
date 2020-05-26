d501ad9229148dbfd126c4057afb178e7fdcd31f
Michael <mmcintyre@ebsco.com>
2020-05-25 22:03:36 -0400

    reorganized function order, added a clean_title function, and added a get actor info function

--- movie-scraper.py
+++ movie-scraper.py
@@ -1,6 +1,8 @@
 import datetime
-import requests
+import re
 import json
+
+import requests
 
 START_YEAR = 1874
 END_YEAR = datetime.datetime.today().year
@@ -13,10 +15,43 @@ BASE_PARAMS = {
     "format": "json",
 }
 
+
 # TODO clean up titles (film) (year film)
 # TODO get more metadata from results
 # TODO write parsers to go to page and get acting list
 # TODO clean up that acting list
+
+
+def get_year_results(session, year):
+    """
+    Top level wrapper function to iterate through all the
+    years between the global START_YEAR and END_YEAR and extend
+    the all_titles list. Takes care of page continuations via the
+    cm_continue key in BASE_PARAMS.
+    :param session: a requests session object
+    :param year: a year to find movie categories for
+    :return: a list of all movie data per year
+    """
+    BASE_PARAMS['cmtitle'] = f"Category:{year}_films"
+    BASE_PARAMS['cmcontinue'] = ""
+
+    all_titles = []
+
+    while True:
+        cat_page = get_cat_data(session, BASE_PARAMS)
+        for page_data in get_page_data(cat_page):
+            all_titles.append({
+                'title': clean_title(page_data['title']),
+                'actors': get_actor_data(page_data['pageid'])
+            })
+
+        cm_continue = get_cm_continue(cat_page)
+
+        if cm_continue:
+            BASE_PARAMS['cmcontinue'] = cat_page['continue']['cmcontinue']
+
+        else:
+            return all_titles
 
 
 def get_cm_continue(data):
@@ -57,41 +92,34 @@ def get_page_data(data):
             yield page
 
 
-def get_year_results(session, year):
+def get_actor_data(pageid):
     """
-    Top level wrapper function to iterate through all the
-    years between the global START_YEAR and END_YEAR and extend
-    the all_titles list. Takes care of page continuations via the
-    cm_continue key in BASE_PARAMS.
-    :param session: a requests session object
-    :param year: a year to find movie categories for
-    :return: a list of all movie data per year
+    Takes in a page_id, which is returned from
+    :param pageid:
+    :return:
     """
-    BASE_PARAMS['cmtitle'] = f"Category:{year}_films"
-    BASE_PARAMS['cmcontinue'] = ""
+    return []
 
-    all_titles = []
 
-    while True:
-        cat_page = get_cat_data(session, BASE_PARAMS)
-        all_titles.extend(list(get_page_data(cat_page)))
-        cm_continue = get_cm_continue(cat_page)
-
-        if cm_continue:
-            BASE_PARAMS['cmcontinue'] = cat_page['continue']['cmcontinue']
-
-        else:
-            return all_titles
+def clean_title(title):
+    """
+    Cleans up parentheses in title, such as Title A (2013 film) or
+    Title B (film).  It hasn't been tested on any other type.
+    :param title: a string
+    :return: a cleaned up title string
+    """
+    film_pattern = r"\([0-9]{0,4}\s{0,1}film\)"
+    return re.sub(film_pattern, "", title)
 
 
 def main():
     with requests.session() as session:
         all_data = {}
-        for year in range(START_YEAR, END_YEAR+1):
+        for year in range(START_YEAR, END_YEAR + 1):
             print(f'Getting movies from {year}')
             all_data[year] = get_year_results(session, year)
 
-    with open('./data/all_movies.json', 'w', encoding='utf-8') as json_out:
+    with open('./data/all_movies.json', 'w') as json_out:
         json.dump(all_data, json_out, indent=4)
 
 
