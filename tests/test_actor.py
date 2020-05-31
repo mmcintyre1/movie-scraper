fd15b97bffa772e01d3aa831cf630298053aaac0
Michael <mmcintyre@ebsco.com>
2020-05-30 21:54:29 -0400

    added some more tests

--- tests/test_actor.py
+++ tests/test_actor.py
@@ -5,7 +5,9 @@ import movie_scraper
 
 @pytest.mark.parametrize(
     "test_input,expected", [
-        ("Raymond Blathwayt – Sir Hugh", "Raymond Blathwayt")  # important to test em-dash vs en-dash
+        ("Raymond Blathwayt – Sir Hugh", "Raymond Blathwayt"), # important to test em-dash vs en-dash
+        ("Roseanne Barr", "Roseanne Barr"),
+        ("Erika Alexander       \tas\t Selma Cotter", "Erika Alexander")
     ]
 )
 def test_clean_actor(test_input, expected):
